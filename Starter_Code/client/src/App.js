0bc3f63110f8d7a87136c629153efb3010763e89
corbin.felsheim@gmail.com <corbinfelsheim@Corbins-MacBook-Pro.local>
2019-04-28 13:11:31 -0700

    adding css

--- Starter_Code/client/src/App.js
+++ Starter_Code/client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Lobby from "./components/lobby/lobby"
+import DraftMinion from "./components/draftMinion/draftMinion";
 
 class App extends Component {
   render() {
@@ -15,7 +16,7 @@ class App extends Component {
       //     To get started, edit <code>src/App.js</code> and save to reload.
       //   </p>
       // </div>
-      <Lobby></Lobby>
+      <DraftMinion></DraftMinion>
     );
   }
 }
